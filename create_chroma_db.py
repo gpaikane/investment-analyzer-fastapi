3fcdffdb6fc956aa161f6ce9bacc6862323811d4
gpaikane <guruppk46@gmail.com>
2025-04-20 12:54:36 +0530

    modified vector store

--- create_chroma_db.py
+++ create_chroma_db.py
@@ -1,4 +1,4 @@
-from langchain.vectorstores import Pinecone
+from langchain.vectorstores import Chroma
 from langchain.embeddings import OpenAIEmbeddings
 from langchain_community.document_loaders import CSVLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -26,17 +26,13 @@ def initialise_chroma_db():
 
     embeddings = OpenAIEmbeddings()
 
-    persist_directory = None
-
     print("creating new chroma vector store")
-    vectorstore = Pinecone.from_documents(
+    vectorstore = Chroma.from_documents(
         documents=split_docs,
-        embedding=embeddings,
-        persist_directory=persist_directory
-
+        embedding=embeddings
     )
 
-    print("vector_store_len")
+    print("vector_store_len", vectorstore._collection.count())
 
     return vectorstore
     # Def
